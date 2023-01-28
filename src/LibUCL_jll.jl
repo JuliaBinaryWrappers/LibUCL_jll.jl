b01d38333fc2b1d3167b240d817aca480de6416d
jlbuild <juliabuildbot@gmail.com>
2023-01-28 18:41:00 +0000

    LibUCL_jll build 0.8.2+0

---
+++ src/LibUCL_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule LibUCL_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("LibUCL")
+JLLWrappers.@generate_main_file("LibUCL", UUID("4559eb44-f4ba-5e76-9185-3ac873597822"))
+end  # module LibUCL_jll
